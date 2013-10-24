32b805803847997570fff7ee627477e5ffea36f1
chuzheng <chuzheng@gmail.com>
2013-10-24 16:20:15 +0800

    change to one global

--- toolkit.js
+++ toolkit.js
@@ -1,49 +1,56 @@
-var Toolkit = {}
+var Toolkit = {};
 
-(function () {
-    // get url args -- clinet
-    function get_query_str_args(args_name) {
-        var qs = location.search.length > 0 ? location.search.substring(1) : "",
-            args = {},
-            items = qs.length ? qs.split("&") : [],
-            item = null,
-            name = null,
-            value = null,
-            i = 0,
-            len = items.length;
+// get url args -- clinet
+Toolkit.get_query_str_args = function (args_name) {
+    var qs = location.search.length > 0 ? location.search.substring(1) : "",
+        args = {},
+        items = qs.length ? qs.split("&") : [],
+        item = null,
+        name = null,
+        value = null,
+        i = 0,
+        len = items.length;
 
-        for (i = 0; i < len; i++) {
-            item = items[i].split("=")
-            name = decodeURIComponent(item[0])
-            value = decodeURIComponent(item[1])
+    for (i = 0; i < len; i++) {
+        item = items[i].split("=")
+        name = decodeURIComponent(item[0])
+        value = decodeURIComponent(item[1])
 
-            if (name.length) {
-                args[name] = value;
-            }
+        if (name.length) {
+            args[name] = value;
         }
-
-        if (typeof args_name === 'undefined') {
-            return args
-        }
-
-        return args[args_name] 
     }
 
-    Toolkit.get_query_str_args = get_query_str_args
-
-    // select random num in range -- both client and server
-    function select_from(lower_val, upper_val) {
-        var choices = upper_val - lower_val + 1;
-
-        return Math.floor(Math.random() * choices + lower_val)
+    if (typeof args_name === 'undefined') {
+        return args
     }
 
-    Toolkit.select_from = select_from
+    return args[args_name] 
+}
 
-    // -- server
-    function is_image(file_type) {
-        return /image/.test(file_type)
+// select random num in range -- both client and server
+Toolkit.select_from = function (lower_val, upper_val) {
+    var choices = upper_val - lower_val + 1;
+
+    return Math.floor(Math.random() * choices + lower_val)
+}
+
+// -- server
+Toolkit.is_image = function (file_type) {
+    return /image/.test(file_type)
+}
+
+// -- both
+Toolkit.is_pair = function (value1, value2) {
+    if (typeof value1 === 'undefined' && typeof value2 !== 'undefined') {
+        return false
     }
 
-    Toolkit.is_image = is_image
-})
+    if (typeof value1 !== 'undefined' && typeof value2 === 'undefined') {
+        return false
+    }
+
+    return true
+}
+
+exports.tool = Toolkit;
