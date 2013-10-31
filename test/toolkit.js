5cd63022915a193567304200ce2428008d0ee7f6
chuzheng <chuzheng@gmail.com>
2013-10-31 13:24:21 +0800

    add test code'

---
+++ test/toolkit.js
@@ -0,0 +1,22 @@
+var toolkit = require('../lib/toolkit'),
+    should = require('should');
+
+describe('toolkit', function () {
+    describe('this Object', function () {
+        it('should be an Object', function() {
+            toolkit.should.instanceof(Object);
+        })
+    })
+
+    describe('#display_long_str_to_short(str, length, type)', function() {
+        it('should keep first 3 chars of chuzheng', function() {
+            toolkit.display_long_str_to_short('chuzheng', 3).should.exactly('chu...');
+        })
+    })
+
+    describe('#display_long_str_to_short(str, length, type)', function() {
+        it('should keep last 3 chars of chuzheng', function() {
+            toolkit.display_long_str_to_short('chuzheng', 3,'last').should.exactly('...eng');
+        })
+    })
+})
